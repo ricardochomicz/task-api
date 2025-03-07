f637d5e371dfaf63a84bdf84cb1f5ca07d7affb0
Ricardo <ricardo.chomicz@gmail.com>
2025-03-07 18:43:12 -0300

    feat: contructor UserService

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function __construct(
+        protected UserService $userService
+    ) {}
     /**
      * Display a listing of the resource.
+     * @return ResourceCollection
      */
     public function index()
     {
-        $users = User::where('id', Auth::id())->get();
+        $users = $this->userService->index();
         return UserResource::collection($users);
     }
 
