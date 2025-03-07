2f1802931c32598f5d5493e590681f32171ab43a
Ricardo <ricardo.chomicz@gmail.com>
2025-03-07 17:01:19 -0300

    feat: add rules tasks

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::get('logout', [AuthController::class, 'logout']);
 
-Route::resource('users', UserController::class)->middleware('auth:api');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+
+    Route::resource('users', UserController::class);
+
+    Route::resource('tasks', TaskController::class);
+});
